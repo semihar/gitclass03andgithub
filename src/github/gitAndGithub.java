6630b16f2dff849d4a5703bbc52d50a55813ecc4
semihar <semiharalem@gmail.com>
2022-05-31 19:54:33 -0400

    2nd comit

--- src/github/gitAndGithub.java
+++ src/github/gitAndGithub.java
@@ -5,6 +5,8 @@ public class gitAndGithub {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+	
+	System.out.print("today we learn github"); 
+	System.out.println("today is a very good day");
 	}
-
 }
